00c9ba6ae08e3c7476a56e6a9cb085e1886a8438
Maksym Telychko <maksym.telychko@gmail.com>
2020-07-01 15:31:07 +0300

    [build] build and deploy scripts

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ARDUINO_HOME="$HOME/.local/opt/arduino"
+
+PATH="$ARDUINO_HOME:$PATH"
+
+mkdir -p build
+
+arduino-builder                                                                \
+    -hardware "$ARDUINO_HOME/hardware"                                         \
+    -tools "$ARDUINO_HOME/tools"                                               \
+    -fqbn "arduino:avr:nano"                                                   \
+    -libraries "$ARDUINO_HOME/libraries"                                       \
+    -prefs "compiler.path=$ARDUINO_HOME/hardware/tools/avr/bin/"               \
+    -prefs "tools.ctags.path=$ARDUINO_HOME/tools-builder/ctags/5.8-arduino11/" \
+    -verbose                                                                   \
+    -build-path build \
+    hydro-suspension.ino
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ARDUINO_HOME="$HOME/.local/opt/arduino"
+
+$ARDUINO_HOME/hardware/tools/avr/bin/avrdude             \
+    -v -D                                                \
+    -C $ARDUINO_HOME/hardware/tools/avr/etc/avrdude.conf \
+    -P /dev/ttyUSB2                                      \
+    -b 57600                                             \
+    -c arduino                                           \
+    -p m328p                                             \
+    -U flash:w:build/hydro-suspension.ino.hex:i
