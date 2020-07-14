0b1a2bfc8a663c7a9f212c54e20a7bda1f7beb9c
Valeriya Reva <valeriyareva@Valeriyas-MacBook-Pro.local>
2020-07-14 09:10:02 +0300

    added comments to test_Led

--- test_Led/test_Led.ino
+++ test_Led/test_Led.ino
@@ -1,35 +1,42 @@
 String WARNING_TEXTS[2] = {
-  "value is correct",
-  "be careful this value not in range"
+  "Value is correct",
+  "Be careful this value not in range"
 };
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
 }
 
 void loop() {
   turnLedAccordingAnalogValue(200, 220, A0, 1000);
 }
 
+/* 
+ * method turnLedAccordingAnalogValue - turn LED and print value
+ * param 'min' - min possible value,
+ * param 'max' - max possible value,
+ * param 'port' - analog port for read values, 
+ * param 'delay' - delay between request in MS,
+ */
 void turnLedAccordingAnalogValue(int minValue, int maxValue, char port, int delayMs) {
   int sensorValue = analogRead(port);
   int ledValue = getLedValue(minValue, maxValue, sensorValue);
 
-  Serial.println(sensorValue);
-  Serial.println(getWarningText(ledValue));
+  printMessages(sensorValue, ledValue);
   digitalWrite(LED_BUILTIN, ledValue);
 
   delay(delayMs);
 }
 
 int getLedValue(int minValue, int maxValue, int value) {
-  if (value > maxValue | value < minValue) {
-    return HIGH;
-  } else {
-    return LOW;
-  }
+  return value > maxValue | value < minValue ? HIGH : LOW;
 }
 
 String getWarningText(int value) {
   return WARNING_TEXTS[value];
+}
+
+void printMessages(int value, int messageIndex) {
+  Serial.println(value);
+  Serial.println(getWarningText(messageIndex));
 }
